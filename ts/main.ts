544ca6d7095986fc92631b90a71046ebae9047aa
Abdurauf <aabdurauf004@gmail.com>
2024-07-11 19:41:49 +0500

    refactor

--- ts/main.ts
+++ ts/main.ts
@@ -2,10 +2,13 @@
 const navBtn = document.querySelector(".burger") as HTMLDivElement;
 const nav = document.querySelector("nav") as HTMLElement;
 const navLinks = document.querySelectorAll(".nav-link");
+const header = document.querySelector("header") as HTMLElement;
+const burger = document.querySelector(".burger") as HTMLDivElement;
 
 // navbar toggler
 navBtn.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
+    burger.classList.toggle("burger-open")
 });
 
 // links to section
@@ -18,19 +21,21 @@ function goToSection(e: Element) {
         e.preventDefault();
         const id: string = e.target.getAttribute("href").slice(1);
         const section: any = document.getElementById(id);
-        const navHeight: number = nav.getBoundingClientRect().height;
-        let position = section.offsetTop - navHeight;
+        const headerHeight: number = header.getBoundingClientRect().height;
+        let position = section.offsetTop - headerHeight;
         window.scrollTo({
-            left: 0,
             top: position,
             behavior: "smooth",
         });
+        nav.classList.remove("nav-active")
+        burger.classList.remove("burger-open")
     });
 }
 
 const closeNavbar = (e: Event) => {
     if (!navBtn.contains(e.target as Node) && !nav.contains(e.target as Node)) {
         nav.classList.remove("nav-active")
+        burger.classList.remove("burger-open")
     }
 }
 
